4bba9e57508aa3eb9e86a4f2184344e337fd1772
Saad Vakharia <saadvakharia128@gmail.com>
2026-06-24 21:05:48 +0530

    feat: implement SceneRoot component for 3D environment orchestration and add contact page styling

--- src/components/canvas/SceneRoot.jsx
+++ src/components/canvas/SceneRoot.jsx
@@ -1,8 +1,15 @@
 import { useEffect } from 'react'
 import { useThree } from '@react-three/fiber'
+import { useTexture } from '@react-three/drei'
 import { Nebula } from '../three/environment/Nebula'
 import { Stars } from '../three/environment/Stars'
 import { IntroScene } from '../three/intro/IntroScene'
+
+// Preload 2D planet textures so the 3D loader tracks them
+useTexture.preload('/textures/playstation_planet.png')
+useTexture.preload('/textures/driving_planet.png')
+useTexture.preload('/textures/vr_planet.png')
+useTexture.preload('/textures/pc_planet.png')
 
 export function SceneRoot({ scrollProgress = 0 }) {
   const { gl } = useThree()
